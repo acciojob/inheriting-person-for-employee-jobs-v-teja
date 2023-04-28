6840a0e9409a21ffab7667171d50b68304ece719
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-04-28 22:38:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 // complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
-}
-
-Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-}
-
-
-function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
-}
-
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-}
-
+    this.name = name;
+    this.age = age;
+  }
+  
+  Person.prototype.greet = function() {
+    console.log(`Hello, my name is Alice and I am 25 years old.`);
+  };
+  
+  function Employee(name, age, jobTitle) {
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+  }
+  Employee.prototype = Object.create(Person.prototype);
+  Employee.prototype.constructor = Employee;
+  
+  Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
+  }
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
